8be46f98d024e2df873cd74760b0c9c433ddb743
Brian Turner <brianturn@gmail.com>
2017-03-07 13:34:30 -0500

    Add files via upload

---
+++ editors/egg.py
@@ -0,0 +1,4 @@
+print ("This file created in emacs.")
+print ("This file created by Brian Turner.")
+print ("I like eggs for breakfast.")
+ 
---
+++ editors/vinegar.py
@@ -0,0 +1,3 @@
+print ("This file created with vim.")
+print ("This file created by Brian Turner.")
+print ("I do not like vinegar.")
