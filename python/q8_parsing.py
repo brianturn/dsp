97ef6f2d23639ddf41b20b60b821e99fb2f56190
Brian Turner <brianturn@gmail.com>
2017-03-12 14:08:53 -0400

    code for parsing football scores

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -4,3 +4,18 @@
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
+def points_diff(filename):
+	d = dict()
+	for line in filename:
+		columns = line.strip().split(',')
+		if columns[0] != 'Team':
+			d[columns[0]] = abs(int(columns[-2]) - int(columns[-3]))
+	return d
+
+def sort_dict():
+	d = points_diff(filename)
+	results = list(d.items())
+	return sorted(results, key=lambda points: points[1])[0]
+
+filename = open('football.csv', 'r')
+print (sort_dict())
