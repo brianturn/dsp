8c7fec4a0cd1985e4fa47a07232f855ba29fa3d8
Brian Turner <brianturn@gmail.com>
2017-03-16 09:20:22 -0400

    write emails to csv file

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,31 @@
-PLACE YOUR CODE HERE
+import csv
+import re
+
+# Using regular expressions
+write_file = open('emails.csv', 'w')
+f = open('faculty.csv', 'r')
+
+for line in f:
+	email = re.search('[A-Za-z]*@\S*[a-zA-Z]', line)
+	if email:
+		write_file.write((email.group() + '\n'))
+write_file.close()
+
+# Using csv reader:
+with open('faculty.csv') as file:
+	reader = csv.reader(file, delimiter=',')
+	header = next(reader)
+
+	emails = []
+	for row in reader:
+		email = row[3]
+		emails.append(email)
+
+write_file = open('emails.csv', 'w')
+writer = csv.writer(write_file)
+
+for i in range(len(emails)):
+	email = emails[i]
+	writer.writerow([email])
+
+write_file.close()
